2e4bfdb33b34bee23b5b716da30e781e0d0ece7d
Kiryl_Sacheuka <kiryl_sacheuka@epam.com>
2021-11-20 14:21:23 +0300

    commit

--- business-service/one/src/main/java/guest/service/repository/GuestRepository.java
+++
@@ -1,9 +0,0 @@
-package guest.service.repository;
-
-import guest.service.domain.Guest;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GuestRepository extends CrudRepository<Guest, Long> {
-}
--- business-service/two/src/main/java/room/service/service/RoomService.java
+++
@@ -1,9 +0,0 @@
-package room.service.service;
-
-import room.service.domain.Room;
-
-public interface RoomService {
-
-    Iterable<Room> getAllRooms();
-    Room getRoomById(long id);
-}
